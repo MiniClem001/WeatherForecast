9901ffab3fbb3cf3cdbf3f74005a5c7665cb1c4e
Clément Giraud-Sauveur <git@milouki.ovh>
2025-03-24 15:15:31 +0100

    extracted views to be reusable and added constants déclaration

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -10,31 +10,31 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack{
-            VStack {
-                Text("Mon")
-                Image(systemName: "sun.max.fill")
-                    .imageScale(.large)
-                    .aspectRatio(contentMode: .fit)
-                    .foregroundColor(.yellow)
-                Text("High: 70°")
-                Text("Low: 50°")
-            }
-            .padding()
-            
-            VStack {
-                Text("Tue")
-                Image(systemName: "cloud.rain.fill")
-                    .imageScale(.large)
-                    .aspectRatio(contentMode: .fit)
-                    .foregroundColor(.blue)
-                Text("High: 60°")
-                Text("Low: 40°")
-            }
-            .padding()
+            DayForecast(day: "Mon", color: Color.yellow, symbol: "sun.max.fill", high: 70, low: 50)
+            DayForecast(day: "Tue", color: Color.blue, symbol: "cloud.rain.fill", high: 60, low: 40)
         }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct DayForecast: View {
+    let day: String
+    let color: Color
+    let symbol: String
+    let high: Int
+    let low : Int
+    
+    var body: some View {
+        VStack {
+            Text(day)
+            Image(systemName: symbol)
+                .foregroundStyle(color)
+            Text("High: \(high)°")
+            Text("Low: \(low)°")
+        }
+        .padding()
+    }
 }
